4fbb330545496bec56dfc4db2dc9d746cf4e7178
niroberto <robertwilly668@gmail.com>
2022-02-25 17:32:14 +0200

    add  is  complete  feature

--- js/script.js
+++ js/script.js
@@ -6,45 +6,60 @@ const items = [];
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  items.push(input.value);
-  let task  =  document.createElement("article");
-  let text = document.createElement("h5");
- var checkBox = cb = document.createElement( "input" );
-cb.type = "checkbox";
-cb.id = "c1";
-cb.value = name;
-  cb.checked = false;
+  if (input.value) {
+    
+    items.push(input.value);
+    let task = document.createElement("article");
+    let text = document.createElement("h5");
+    var checkBox = cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.id = "c1";
+    cb.value = name;
+    cb.checked = false;
   
-  let textNode = document.createElement("h5");
-  let rem  = document.createElement("div");
-  let up  = document.createElement("div");
-  text.appendChild(textNode);
-  text.classList.add("text");
-task.appendChild(cb);
+    let textNode = document.createElement("h5");
+    let rem = document.createElement("div");
+    let up = document.createElement("div");
+    text.appendChild(textNode);
+    text.classList.add("text");
+    task.appendChild(cb);
 
-task.appendChild(text);
-task.appendChild(up);
-task.appendChild(rem);
-  allItems.appendChild(task);
-  rem.textContent = "Remove";
-  up.textContent = "Update";
-  rem.classList.add("remove");
-  up.classList.add("update");  
-  items.map((el)=>{
-    text.textContent = el;
-  })
-  console.log(task);
-  rem.addEventListener("click", (e) => {
-     e.preventDefault();
+    task.appendChild(text);
+    task.appendChild(up);
+    task.appendChild(rem);
+    allItems.appendChild(task);
+    rem.textContent = "Remove";
+    up.textContent = "Update";
+    rem.classList.add("remove");
+    up.classList.add("update");
+    up.addEventListener("click", () => {
+      input.value = up.parentElement.firstChild.nextSibling.textContent;
+      btn.textContent = 'Update';
+      let index = items.indexOf(up.parentElement.firstChild.nextSibling.textContent);
+      console.log(index);
+
+      btn.addEventListener('click', () => {
+        // items[index] = input.value;
+        items.splice(index, 1, input.textContent);
+        console.log(items);
+      })
+    })
+    items.map((el) => {
+      text.textContent = el;
+    })
+    console.log(task);
+    rem.addEventListener("click", (e) => {
+      e.preventDefault();
+      allItems.removeChild(rem.parentElement);
+    });
+    let isComplete = false;
+
     checkBox.addEventListener('click', () => {
-       
-     })
-
-    // allItems.removeChild( rem.parentElement)
-  });
-  // cd.addEventListener('click', () => {
-  //   e.preventDefault();
-
-  // })
-
+      console.log(checkBox.parentElement);
+      isComplete = !isComplete;
+      checkBox.parentElement.firstChild.nextSibling.style.textDecoration = isComplete ? "line-through" : "";
+      checkBox.parentElement.firstChild.nextSibling.style.color = isComplete ? "grey" : "";
+    })
+    input.value = "";
+  }
 });
